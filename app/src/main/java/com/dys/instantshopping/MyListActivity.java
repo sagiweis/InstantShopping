2822e207c5ad20e6856524b9c6da7c8887b660d8
dalbagly <dalbagly@gmail.com>
2016-05-02 00:18:28 +0300

    2 new activities of a shopping list and add item

---
+++ app/src/main/java/com/dys/instantshopping/MyListActivity.java
@@ -0,0 +1,34 @@
+package com.dys.instantshopping;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MyListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("listName");
+        setTitle(title);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MyListActivity.this, AddItemActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+}
